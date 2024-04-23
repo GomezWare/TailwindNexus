555101e0dfbaed4594c9c1f6ff83d03fc2264fe5
GomezWare <streetninjacoder@gmail.com>
2024-04-23 22:22:59 +0200

    Added the astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
 });
