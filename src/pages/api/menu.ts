db11249e555ff04263e0f292c9d463a58e9423d1
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-07 10:50:14 +0200

    docs(Endpoints): Added documentations to endpoints

--- src/pages/api/menu.ts
+++ src/pages/api/menu.ts
@@ -1,8 +1,20 @@
-import type { APIRoute } from 'astro'
-import { getMenu } from '@services/categories' 
+/**
+ * 
+*/
 
+// Imports
+import type { APIRoute } from 'astro'
+import { getMenu } from '@services/categories'
+
+// REST API Route
+
+// GET
 export const GET: APIRoute = () => {
+  
+  // Create the response with the data that the service returned (the JSON itself)
+
+  // API Response
   return new Response(
-   JSON.stringify(getMenu())
+    JSON.stringify(getMenu())
   )
 }
