a7416b587902cd6433d9a1bba9b444594416cd72
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-07 10:02:11 +0200

    Category + endopint + controller working

--- src/pages/api/category/[id].ts
+++ src/pages/api/category/[id].ts
@@ -3,11 +3,6 @@ import {getCategory} from '@services/categories'
 
 export const GET: APIRoute = ({params}) => {
     const id = params.id
-    const response = getCategory(id)
-
-if(!response){
-    return new Response(null, {status:400, statusText:'Not Found'})
-}
-return new Response(JSON.stringify(response))
-
+    const response = getCategory(id) || []
+return new Response(JSON.stringify(response));
 }
