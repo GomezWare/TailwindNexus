0e30271929374b460054b712f5ae733b57356e95
GomezWare <streetninjacoder@gmail.com>
2024-05-11 17:40:06 +0200

    Created scripts for Docker

--- docker/createMySqlContainer.sh
+++ docker/createMySqlContainer.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
-# echo "Choose MySQL root password: "
-# read -s pass1
+!/bin/bash
+ echo "Choose MySQL root password: "
+ read -s pass1
 
 
-# echo "Enter the password again: "
-# read -s pass2
+ echo "Enter the password again: "
+ read -s pass2
 
-# if [ "$pass1" == "$pass2" ]; then
+ if [ "$pass1" == "$pass2" ]; then
 docker run --name tnexus-mysql \
-  -e MYSQL_ROOT_PASSWORD=password \
+  -e MYSQL_ROOT_PASSWORD=$pass1 \
   -e MYSQL_AUTHENTICATION_PLUGIN=mysql_native_password \
   -p 3306:3306 \
   -v ./scripts:/docker-entrypoint-initdb.d \
   -d mysql
 
-# else
-#   echo "Passwords do not match. Please try again."
-#   exit 1
-# fi
-
+ else
+   echo "Passwords do not match. Please try again."
+   exit 1
+ fi
