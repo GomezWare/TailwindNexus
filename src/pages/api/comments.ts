11a72039d859d36fcbcc38a3556959a8188451b2
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-15 10:02:09 +0200

    Comments + deleted faluty code

--- src/pages/api/comments.ts
+++ src/pages/api/comments.ts
@@ -10,29 +10,9 @@ import { validateComponent } from "@utils/validateComponent";
 
 // REST API Route
 
-// GET
-export const GET: APIRoute = async () => {
-  // Create the response with the data that the service returned (the JSON itself)
-  const response = (await getComponents()) || [];
-
-  // If data is succefully retrieved send the data else send a 500 error response
-  if (response) {
-    // API Response
-    return new Response(JSON.stringify(response));
-  } else {
-    // If the response fails respond with a 500 server status
-    return new Response(JSON.stringify([]), {
-      status: 500,
-      statusText: "Internal server error",
-    });
-  }
-};
-
 // POST
 export const POST: APIRoute = async ({ request }) => {
-  // TODO call a function to generate the Thumbnail and get the route
   // TODO Protect endpoints with JWT
-  // TODO Generate thumbnail
   try {
     // If the POST body is not a JSON
     if (request.headers.get("Content-Type") === "application/json") {
