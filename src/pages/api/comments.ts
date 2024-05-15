c072d562b74a88599d00c8f06e17b1bdd614fc41
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-15 12:23:53 +0200

    Comment form

--- src/pages/api/comments.ts
+++ src/pages/api/comments.ts
@@ -5,59 +5,11 @@
 
 // Imports
 import type { APIRoute } from "astro";
-import { getComponents, addComponent } from "@services/components";
-import { validateComponent } from "@utils/validateComponent";
 
 // REST API Route
 
 // POST
 export const POST: APIRoute = async ({ request }) => {
-  // TODO Protect endpoints with JWT
-  try {
-    // If the POST body is not a JSON
-    if (request.headers.get("Content-Type") === "application/json") {
-      // Getting the JSON
-      const body = await request.json();
-      // Component data array
-      const component = {
-        categoryId: body.categoryId,
-        userId: 1,
-        name: body.name,
-        description: body.description,
-        thumbnail: "https://ximg.es/300x200",
-        needsAlpine: Boolean(body.needsAlpine),
-        needsCDN: Boolean(body.needsCDN),
-        tailwind: body.tailwind,
-        javascript: body.javascript,
-      };
-
-      // Validate data and make the request to the database
-      if (validateComponent(component)) {
-        return new Response(
-          JSON.stringify(
-            (await addComponent([
-              component.categoryId,
-              component.userId,
-              component.name,
-              component.description,
-              component.thumbnail,
-              component.needsAlpine,
-              component.needsCDN,
-              component.tailwind,
-              component.javascript,
-            ])) || []
-          )
-        );
-      } else {
-        return new Response(JSON.stringify("Validation Failed"), {
-          status: 403,
-          statusText: "Validation Failed",
-        });
-      }
-    }
-    return new Response(null, { status: 400, statusText: "Bad Request" });
-  } catch (err) {
-    console.log(err);
-    return new Response(null, { status: 500, statusText: "Server error" });
-  }
+  // TODO Protect endpoints with JWT (User verification)
+ return new Response();
 };
