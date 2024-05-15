48672b74e51425c906a6a8cb1f8d55f3f80df061
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-15 13:19:16 +0200

    Added the function to add comments

--- src/pages/api/comments.ts
+++ src/pages/api/comments.ts
@@ -4,6 +4,7 @@
  */
 
 // Imports
+import { addComment } from "@services/comments";
 import type { APIRoute } from "astro";
 
 // REST API Route
@@ -11,5 +12,50 @@ import type { APIRoute } from "astro";
 // POST
 export const POST: APIRoute = async ({ request }) => {
   // TODO Protect endpoints with JWT (User verification)
- return new Response();
+
+  try {
+    // If the POST body is not a JSON
+    if (request.headers.get("Content-Type") === "application/json") {
+      // Getting the JSON
+      const body = await request.json();
+
+      // Comment object
+      const comment = {
+        userId: 1,
+        componentId: body.componentId,
+        content: body.content,
+      };
+
+      // Checks if comment content is empty or component id is empty else return 400 response
+      if (String(comment.content).trim() == "" || !comment.componentId) {
+        return new Response(JSON.stringify([]), {
+          status: 400,
+          statusText: "bad request",
+        });
+      }
+
+      // Call component insert fucntion to insert Data into the DB
+      const response = await addComment([
+        comment.userId,
+        comment.componentId,
+        comment.content,
+      ]);
+
+      // Return the response with 200 status
+      return new Response(JSON.stringify({ inserted: response }));
+    } else {
+      // API Response with bad request status
+      return new Response(JSON.stringify([]), {
+        status: 400,
+        statusText: "bad requests",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    // API Response with bad request status
+    return new Response(JSON.stringify([]), {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
 };
