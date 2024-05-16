afb1d2e221a59fd12cb28c0cc3819b66b6221782
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-16 14:51:20 +0200

    Auth implemented by github

---
+++ src/libs/services/auth.ts
@@ -0,0 +1,61 @@
+// TODO Document this
+
+// Imports
+import { dbQuery } from "@utils/dbQuery";
+
+// These functions can be used from Astro pages
+
+/**
+ * This function register a user using the session data and its called when we cant find
+ * the email in the user database.
+ *
+ * @async
+ * @param {String} name
+ * @param {String} avatar
+ * @param {String} email
+ * @return {boolean}
+ */
+const registerUser = async (name, email, avatar) => {
+  try {
+    // Register user
+    const [userDataRows, userDataFields] = await dbQuery(
+      `
+      INSERT INTO users (name, email, password, avatar, created_at, updated_at) 
+      VALUES (?, ?, "", ?, UNIX_TIMESTAMP(), UNIX_TIMESTAMP());`,
+      [name, email, avatar]
+    );
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+/**
+ * Check if the email exist in the db and return user id, if user doesnt exists returns a negative ID
+ * @async
+ * @param {String} mail
+ * @returns {Number}
+ */
+const checkUserMail = async (mail) => {
+  try {
+    const [userDataRows, userDataFields] = await dbQuery(
+      `
+      SELECT user_id
+      FROM users
+      WHERE email = ?;
+      `,
+      [mail]
+    );
+    if (userDataRows.length == 0) {
+      return -1;
+    } else {
+      return userDataRows[0].user_id;
+    }
+  } catch (error) {
+    console.log(error);
+    return -1;
+  }
+};
+
+export { registerUser, checkUserMail };
