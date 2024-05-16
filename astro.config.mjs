ba792c5082d4cda30a2581c7c3fd06324e9224ef
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-16 11:55:00 +0200

    Node integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
   devToolbar: {
-    enabled: false,
+    enabled: false
   },
+  adapter: node({
+    mode: "standalone"
+  })
 });
