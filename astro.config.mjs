cfeab3d576a93a8ff3cd889b5a8bff036bcf6cc2
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-16 11:56:00 +0200

    Node + Auth astro implmentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), auth()],
   output: "server",
   devToolbar: {
     enabled: false
