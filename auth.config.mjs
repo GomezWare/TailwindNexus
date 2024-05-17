9573125d67ea8e2ace1ee02fb7bbd7deedfdf7a4
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-17 12:43:30 +0200

    Commends page from uodate

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,4 +1,6 @@
+import Facebook from "@auth/core/providers/facebook";
 import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
 import { defineConfig } from "auth-astro";
 
 export default defineConfig({
@@ -6,6 +8,14 @@ export default defineConfig({
     GitHub({
       clientId: import.meta.env.GITHUB_CLIENT_ID,
       clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: import.meta.env.GOOGLE_CLIENT_ID,
+      clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Facebook({
+      clientId: import.meta.env.FACEBOOK_CLIENT_ID,
+      clientSecret: import.meta.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
 });
