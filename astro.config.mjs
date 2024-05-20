d56cef0bc0a2f9d5c94b7c9933b3f85a7e95f76a
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-20 12:38:08 +0200

    Editor improved

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-
 import auth from "auth-astro";
 
 // https://astro.build/config
