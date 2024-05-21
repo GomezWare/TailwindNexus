3912328f8e95603f16010bcd2c5fac54fb35fddd
David Gómez <146298390+GomezWare@users.noreply.github.com>
2024-05-21 13:28:16 +0200

    Editor implemented on component preview

--- src/components/partials/editor/CodeMirror.jsx
+++ src/components/partials/editor/CodeMirror.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { html } from "@codemirror/lang-html";
+import { dracula } from "@uiw/codemirror-theme-dracula";
 
-function App() {
-  const [value, setValue] = React.useState(`console.log('hello world!');console.log('hello world!');console.log('hello world!');console.log('hello world!);`);
+function App(props) {
+  const [value, setValue] = React.useState(
+    `console.log('hello world!');console.log('hello world!');console.log('hello world!');console.log('hello world!);`
+  );
   const onChange = React.useCallback((val, viewUpdate) => {
-    console.log('val:', val);
-    setValue(val);
+    console.log("val:", val);
   }, []);
-  return <CodeMirror value={value} height="200px" extensions={[javascript({ jsx: true })]} onChange={onChange} indentWithTab={true} readOnly={true}/>;
+  return (
+    <CodeMirror
+      value={props.value}
+      extensions={[
+        javascript({ jsx: true }),
+        html({ autoCloseTags: true, extraTags: true }),
+      ]}
+      height={props.height}
+      onChange={onChange}
+      theme={dracula}
+      readOnly={props.readonly}
+    />
+  );
 }
 export default App;
