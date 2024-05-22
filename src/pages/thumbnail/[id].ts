6ad65e6cbf2168ef0af28beaafc6f7b3a63813ea
GomezWare <streetninjacoder@gmail.com>
2024-05-22 18:17:46 +0200

    added placeholder

--- src/pages/thumbnail/[id].ts
+++ src/pages/thumbnail/[id].ts
@@ -1,21 +1,37 @@
-// TODO Comment this
+/**
+ * After moving the project to production I noticed that the thumbnails
+ * were not working so I created this endpoint which will serve the thumbnails
+ * regardless of whether the project is in development or production.
+ */
 
 import type { APIRoute } from "astro";
 import path from "path";
 import { readFileSync } from "fs";
 
+// GET API ROUTE
 export const GET: APIRoute = async ({ params }) => {
+  // Obtain the thumbnail id via params
   const imageId = params.id;
 
-  const imagePath = path.join(process.cwd(), "upload", imageId);
   try {
-    const image = readFileSync(imagePath);
+    // Try to get the imgae
+    // Calculate the path of the image in the server
+    const image = readFileSync(path.join(process.cwd(), "upload", imageId));
+
+    // Return the thumbnail as binary image png file
     return new Response(image, {
       status: 200,
       headers: { "Content-Type": "image/png" },
     });
   } catch (err) {
+    // If we cant find the image return a 404 response with a placeHolder
+    const image = readFileSync(
+      path.join(process.cwd(), "upload", "placeholder.png")
+    );
     console.log(err);
-    return new Response("Image not found", { status: 404 });
+    return new Response(image, {
+      status: 200,
+      headers: { "Content-Type": "image/png" },
+    });
   }
 };
